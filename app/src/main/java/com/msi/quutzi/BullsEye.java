9319bdb5ff5705ac8ecd40c418f8c04f4a4ce451
Steve Johnson <steve@monktonsoftware.com>
2017-10-30 01:40:11 -0400

    Fixed some bugs in level changing.

--- app/src/main/java/com/msi/quutzi/BullsEye.java
+++ app/src/main/java/com/msi/quutzi/BullsEye.java
@@ -3,31 +3,34 @@ package com.msi.quutzi;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 
-public class BullsEye {
-    private Paint bullsEyePaint;
+import static android.R.attr.x;
+//import static android.view.View.X;
+//import static android.view.View.Y;
+
+class BullsEye {
     private float ballRadius;
-    private float ballX, ballY;
+    private float centerX, centerY;
+    private Paint bullsEyePaint = new Paint();
 
-    public BullsEye() {
-
+    BullsEye() {
+        bullsEyePaint.setColor(Color.BLACK);
+        bullsEyePaint.setStrokeWidth(3);
     }
 
-    public void setPaint(Paint paint) {
-        bullsEyePaint = paint;
-    }
-
-    public void setRadius(float radius) {
+    void setRadius(float radius) {
         ballRadius = radius;
     }
 
-    public void setCoordinates(float x, float y) {
-        ballX = x;
-        ballY = y;
+
+    void setCoordinates(float x, float y) {
+        centerX = x;
+        centerY = y;
     }
 
-    public void draw(Canvas canvas, float bullsEyeAngle) {
+    void draw(Canvas canvas, float bullsEyeAngle) {
         drawOneLine(canvas, bullsEyeAngle);
         bullsEyeAngle = bullsEyeAngle + 90;
         if (bullsEyeAngle > 360) {
@@ -39,10 +42,10 @@ public class BullsEye {
     private void drawOneLine(Canvas canvas, float bullsEyeAngle) {
         float xF = (float) Math.sin(Math.toRadians(bullsEyeAngle));
         float yF = (float) Math.cos(Math.toRadians(bullsEyeAngle));
-        float xStart = ballX - (ballRadius * xF);
-        float xEnd = ballX + (ballRadius * xF);
-        float yStart = ballY - (ballRadius * yF);
-        float yEnd = ballY + (ballRadius * yF);
+        float xStart = centerX - (ballRadius * xF);
+        float xEnd = centerX + (ballRadius * xF);
+        float yStart = centerY - (ballRadius * yF);
+        float yEnd = centerY + (ballRadius * yF);
         canvas.drawLine(xStart, yStart, xEnd, yEnd, bullsEyePaint);
     }
 }
